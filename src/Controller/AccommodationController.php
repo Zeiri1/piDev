8119c516ea8fd22f322a4a986364d22f8f825a90
unknown <ibtihel305@gmail.com>
2024-02-17 21:38:12 +0100

    crud done

--- src/Controller/AccommodationController.php
+++ src/Controller/AccommodationController.php
@@ -22,7 +22,7 @@ class AccommodationController extends AbstractController
         ['Accommodations' => $allAccoms]
     );
     }
-    #[Route('/Accommodation/add', name: 'Accommodation_add')]
+    #[Route('/accommodation/add', name: 'Accommodation_add')]
     public function AddAccommodation(ManagerRegistry $doctrine, Request $request): Response
     {
         $Accommodation =new Accommodation();
@@ -38,4 +38,39 @@ class AccommodationController extends AbstractController
             'formAccom'=>$form->createView(),
         ]);
     }
+     #[Route('/accommodation/delete/{id}', name: 'deleteAccommodation')]
+    public function deleteAccommodation($id, ManagerRegistry $manager, AccommodationRepository $AccomRepo): Response
+    {
+       $emm = $manager->getManager();
+       $id_remove = $AccomRepo->find($id);
+       $emm->remove($id_remove);
+       $emm->flush();
+
+
+       return $this->redirectToRoute('app_accommodation');
+    }
+
+    #[Route('/accommodation/edit/{id}', name: 'editAccommodation')]
+    public function editAccommodation( $id,ManagerRegistry $manager, AccommodationRepository $AccomRepo, Request $request): Response
+    {
+        $em=$manager->getManager();
+        $dataid=$AccomRepo->find($id);
+       // var_dump($dataid).die() ;
+        $form=$this->createForm(AccommodationFormType::class,$dataid) ;
+        $form->handleRequest($request) ; 
+ 
+        if($form->isSubmitted() and $form->isValid()){      //est ce que button qrass aaliha wela et est ceque les camps hatt'hom valid wela 
+ 
+            $em->persist($dataid);                          //t'hadher requet INSERT
+            $em->flush() ;                                   //execute 
+            return $this->redirectToRoute('app_accommodation') ;
+ 
+        }
+ 
+        return $this->renderForm('accommodation/form.html.twig', [
+            'formAccom' => $form,
+        ]);
+    }
+
+
 }
--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -2,22 +2,20 @@
 
 namespace App\Form;
 
-use App\Entity\Accommodation;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AccommodationFormType extends AbstractType
+class CategoryFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Title')
-            ->add('Adress')
-            ->add('Price')
-            ->add('Type')
-            ->add('category')
+            ->add('CategoryName')
+            ->add('Description')
+            ->add('Statistics')
             ->add('add', SubmitType::class)
         ;
     }
@@ -25,7 +23,7 @@ class AccommodationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Accommodation::class,
+            'data_class' => Category::class,
         ]);
     }
 }
